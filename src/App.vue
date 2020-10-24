79e27f0edbecb9852174d10e4f20dd287fb1de39
Erastus Amunwe <eneamunwe@gmail.com>
2020-10-24 02:34:41 +0200

    registered NavBar & Options components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
-<h1>GetBible</h1>
+<div>
+  <navbar/>
+  <div class="uk-container">
+    <options/>
+  </div>
+</div>
 </template>
 
 <script>
+// import _ from 'lodash';
+
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
+
+import navbar from './components/NavBar.vue';
+import options from './components/Options.vue';
 
 UIkit.use(Icons);
  
 export default {
   name: 'App',
   components: {
-  }
+    navbar,
+    options
+  },
 }
 </script>
 
@@ -21,7 +33,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  // text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }

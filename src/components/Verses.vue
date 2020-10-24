df1d167900bee322f2f877de564736e23f8ae423
Erastus Amunwe <eneamunwe@gmail.com>
2020-10-24 02:32:17 +0200

    create Verses' vue component to list all the verses in a chapter received as a prop

---
+++ src/components/Verses.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul :dir="dir" 
+     class="uk-margin-medium-top uk-list uk-list-divider uk-animation-hover">
+        <li  v-for="(verse, i) in chapter" :key="i" 
+        :class="`uk-animation-slide-bottom uk-animation-15 uk-animation-hover`">
+            
+            <h4  class="btn btn-primary">
+                {{book_name}}  
+                <span class="uk-badge badge-light">
+                    {{verse.chapter}}:{{verse.verse}}
+                </span>
+            </h4>
+            {{verse.text}} <br/>
+            <p></p>
+        </li>
+    </ul>
+</template>
+<script>
+export default {
+    props: {
+        dir: {
+            type:String,
+            required:true},
+        book_name: {
+            type:String,
+            required: true,
+        },
+        chapter:{
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>

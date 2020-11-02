f80f015c0e9aeff6560f7177fa85fc7a04cd0ebf
Erastus Amunwe <eneamunwe@gmail.com>
2020-11-02 10:09:20 +0200

    changed default initialisation from functions

---
+++ src/store/state.js
@@ -0,0 +1,12 @@
+export default {
+    saved_translations: [],
+    // test: (()=>(['Hello', 'Wold']))()
+    // ,
+    selected:{
+      translation: null,
+      book: null,
+      chapter: null
+    },
+    in_memory_translation:{},
+    search: '',
+  }

74de9170150067193e16e95dc71578bcdf6a3c07
Erastus Amunwe <eneamunwe@gmail.com>
2020-11-02 10:10:16 +0200

    dived the four store propeties into separate files

---
+++ src/store/store.js
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex'
+
+
+import state from './state';
+import getters from  './getters';
+import mutations from  './mutations';
+import actions from './actions'
+
+export const store = createStore({
+  state,
+  getters,
+  mutations,
+  actions
+})

f0f69805e4fc2f3b7ba40df8f28f2ac8b7392fd4
Erastus Amunwe <eneamunwe@gmail.com>
2021-01-08 15:09:47 +0200

    added search filter's variables

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -7,7 +7,7 @@
                     <div class="uk-margin">
                         <form class="uk-search uk-search-default" action="javascript:void(0)">
                                 <!-- <span uk-search-icon></span> -->
-                                <input class="uk-search-input text-secondary primary rounded" v-model="search" type="search" placeholder="Search Words...">
+                                <input class="uk-search-input text-secondary primary rounded" v-model="search_text" type="search" placeholder="Search Words...">
                                 <div class="uk-margin">
                                     <button class="uk-button uk-button-default uk-button-small text-secondary primary rounded">Search</button>
                                 </div>
@@ -32,6 +32,18 @@
                                 <button class="uk-button uk-button-default uk-button-small text-secondary primary">CHAPTER</button>
                             </div>
                         </div>
+                        <div class="uk-button-group" data-uk-button-radio>
+                            <label class="uk-button primary">
+                                <input class="primary" type="radio" checked="checked" v-model="gender"  value="0" style="display:none;"/>
+                                Male
+                            </label>
+                            <label class="uk-button primary">
+                                <input type="radio"  v-model="gender" value="1" style="display:none;"/>
+                                Female
+                            </label>
+                        </div>
+                        {{gender}}
+                        <!-- <input type="color" name="theme" id=""> -->
                         <div class="uk-margin">
                             <div class="uk-button-group">
                                 <button class="uk-button uk-button-default uk-button-small text-secondary primary uk-active">EXACT MATCH</button>
@@ -53,6 +65,18 @@
 </template>
 <script>
 export default {
-    
+    data(){
+        return {
+            search_text: '', 
+            search_area: '',
+            match: '',
+            sensitivity: '',
+            words: '',
+            gender: '',
+        }
+    },
+    methods: {
+
+    }
 }
 </script>
